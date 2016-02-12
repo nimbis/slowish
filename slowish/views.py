aaa30164e97106d33b5b420ce34363cb55925806
Carl Worth <cworth@cworth.org>
2016-02-12 14:57:38 -0800

    Add the most minimal possible implementation of the tokens view.
    So far, this just unconditionally returns a repsonse indicating the
    user is not authorized.
    
    That's not extremely useful, but it's just enough to pass our first
    test case.

--- slowish/views.py
+++ slowish/views.py
@@ -1,3 +1,13 @@
-# from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+
+@csrf_exempt
+def tokens(request):
+
+    response = {"unauthorized":
+                {"code": 401,
+                 "message":
+                 "Unable to authenticate user with credentials provided."}}
+
+    return JsonResponse(response, status=401)
