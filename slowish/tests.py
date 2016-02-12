95802b286504770ac2a3b51ed27d661cbd7d853d
Carl Worth <cworth@cworth.org>
2016-02-12 14:35:16 -0800

    Add an initial, and very simple, test case.
    This test simply ensures that if we hit the "tokens" view with
    incorrect credentials that we get an unauthorized response.
    
    This test is expected to fail currently, since there is not yet any
    tokens view implemented.

--- slowish/tests.py
+++ slowish/tests.py
@@ -1,3 +1,22 @@
-# from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+from django.test.client import Client
 
-# Create your tests here.
+
+class TokensViewTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_tokens_invalid(self):
+        """Verify that tokens view responds correctly to an invalid post."""
+
+        # This post does not supply any of the required
+        # credentials. It should receive a response indicating that
+        # authentication failed.
+        response = self.client.post(reverse('tokens'))
+        self.assertEquals(response.status_code, 401)
+        self.assertJSONEqual(
+            response.content,
+            '{"unauthorized":{"code":401,"message":'
+            '"Unable to authenticate user with credentials provided."}}')
