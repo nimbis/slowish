cea2849fde734f84cf7bc6e98cf2a15cc29b2122
Carl Worth <cworth@cworth.org>
2016-02-12 16:54:20 -0800

    Add a test for authenticating a user
    The new test creates a user in the database and verifies that the
    "tokens" view correctly authenticates (with the correct password) and
    also that it fails to authenticate (when the password is incorrect).
    
    This new test is expected to fail currently, (since we do not yet have
    code to verify a username and password in the tokens view yet).

--- slowish/tests.py
+++ slowish/tests.py
@@ -1,6 +1,29 @@
+import json
+
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.test.client import Client
+
+from .models import SlowishAccount, SlowishUser
+
+user_data = {
+    "auth": {
+        "passwordCredentials": {
+            "username": "user",
+            "password": "secret",
+        },
+        "tenantId": "1234",
+    }
+}
+
+
+def create_user():
+    account = SlowishAccount.objects.create(
+        id=user_data["auth"]["tenantId"])
+    return SlowishUser.objects.create(
+        account=account,
+        username=user_data["auth"]["passwordCredentials"]["username"],
+        password=user_data["auth"]["passwordCredentials"]["password"])
 
 
 class TokensViewTest(TestCase):
@@ -20,3 +43,31 @@ class TokensViewTest(TestCase):
             response.content,
             '{"unauthorized":{"code":401,"message":'
             '"Unable to authenticate user with credentials provided."}}')
+
+    def test_tokens_authorized(self):
+        """Verify that the tokens view gives a token to an authorized user."""
+
+        self.user = create_user()
+
+        data = user_data
+
+        # Ensure we get a succesful return with valid credentials
+        response = self.client.post(
+            reverse('tokens'),
+            json.dumps(data),
+            content_type="application/json")
+        self.assertEquals(response.status_code, 200)
+        content = json.loads(response.content)
+        url = (content['access']['serviceCatalog'][0]
+               ['endpoints'][0]['publicURL'])
+        self.assertEquals(url, "http://testserver/slowish/files/1234")
+        self.assertEquals(content['access']['token']['id'], self.user.token)
+
+        # Try again, but this time with an invalid password, which
+        # should yield an authorization failure
+        data["auth"]["passwordCredentials"]["password"] = "incorrect"
+        response = self.client.post(
+            reverse('tokens'),
+            json.dumps(data),
+            content_type="application/json")
+        self.assertEquals(response.status_code, 401)
