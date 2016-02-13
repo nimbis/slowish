822c655ff49feff075f9988ce64fb4eeb0b364fa
Carl Worth <cworth@cworth.org>
2016-02-12 17:04:06 -0800

    Implement verification of account.id, username, and password in tokens view.
    This fixes the test that was recently added. In the case of an
    authorized user this view uses a template to return a json-formatted
    response that gives the URL of the endpoint needed for future requests
    and the token that will be needed for those requests.
    
    (Note: The endpoint URL returned is correct, but following it up will
    not currently work since this app does not yet have an implementation
    of the views necessary for the subsequent requests.)

--- slowish/views.py
+++ slowish/views.py
@@ -1,13 +1,46 @@
+from datetime import datetime, timedelta
+import json
+
 from django.http import JsonResponse
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.template import loader
+
+from .models import SlowishUser
+
+
+def unauthorized():
+    unauth_response = {
+        "unauthorized":
+        {"code": 401,
+         "message":
+         "Unable to authenticate user with credentials provided."}}
+
+    return JsonResponse(unauth_response, status=401)
 
 
 @csrf_exempt
 def tokens(request):
 
-    response = {"unauthorized":
-                {"code": 401,
-                 "message":
-                 "Unable to authenticate user with credentials provided."}}
+    try:
+        jreq = json.loads(request.body)
+        account_id = jreq['auth']['tenantId']
+        username = jreq['auth']['passwordCredentials']['username']
+        password = jreq['auth']['passwordCredentials']['password']
+        user = SlowishUser.objects.get(
+            account__id=account_id,
+            username=username,
+            password=password)
+    except:
+        return unauthorized()
 
-    return JsonResponse(response, status=401)
+    expiration = datetime.utcnow() + timedelta(days=2)
+
+    template = loader.get_template('slowish/tokens.json')
+    context = {
+        "user": user,
+        "expires": expiration.strftime("%Y-%m-%dT%H:%M:%SZ"),
+    }
+    return HttpResponse(
+        template.render(context, request),
+        content_type="application/json")
