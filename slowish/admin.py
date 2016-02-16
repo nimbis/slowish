299b3b2be5e57341a16b0a04bbbfa279c8cdb41b
Carl Worth <cworth@cworth.org>
2016-02-16 12:05:41 -0800

    Add a model for a SlowishContainer
    This model has a foreign key to a SlowishAccount since each container
    lives (and is uniquely named) within an account.
    
    This commit adds the generated migration as well, (with a pleasant name).

--- slowish/admin.py
+++ slowish/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import SlowishAccount, SlowishUser
+from .models import SlowishAccount, SlowishUser, SlowishContainer
 
 admin.site.register(SlowishAccount)
 admin.site.register(SlowishUser)
+admin.site.register(SlowishContainer)
--- slowish/models.py
+++ slowish/models.py
@@ -57,3 +57,21 @@ class SlowishUser(models.Model):
 
     def __unicode__(self):
         return "{0} (in account {1})".format(self.username, self.account.id)
+
+
+class SlowishContainer(models.Model):
+    """A container, (within a particular account)."""
+
+    account = models.ForeignKey(SlowishAccount, on_delete=models.CASCADE)
+
+    name = models.CharField(
+        help_text="A name for this container.",
+        max_length=255)
+
+    class Meta:
+        verbose_name = "Slowish Container"
+        verbose_name_plural = "Slowish Containers"
+        unique_together = ('account', 'name')
+
+    def __unicode__(self):
+        return "{0} (in account {1})".format(self.name, self.account.id)
