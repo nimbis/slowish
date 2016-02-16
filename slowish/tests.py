544656dd929d4fa4388c72190808ba2bae68be26
Carl Worth <cworth@cworth.org>
2016-02-16 05:46:12 -0800

    Add a simple test of a proposed 'account' view
    This test verifies that a GET of the "account" view requires a
    correct HTTP_X_AUTH_TOKEN header, (which should be retrieved from a
    POST to the "tokens" view).
    
    This new test fails currently since there is only a stub of the
    "account" view implemented so far.

--- slowish/tests.py
+++ slowish/tests.py
@@ -3,8 +3,11 @@ import json
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.test.client import Client
+from django.test.client import RequestFactory
 
 from .models import SlowishAccount, SlowishUser
+from .views import account
+
 
 user_data = {
     "auth": {
@@ -73,3 +76,40 @@ class TokensViewTest(TestCase):
             reverse('tokens'),
             json.dumps(data), content_type="application/json")
         self.assertEquals(response.status_code, 401)
+
+
+class FilesViewTest(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = create_user()
+
+    # The various files views are a little tricky to invoke. We would
+    # like to just use self.client.get() but we can't because we need
+    # to install a custom HTTP_X_AUTH_TOKEN header. So, instead we use
+    # RequestFactory to assemble the request and then we pass that
+    # request directly into the account() view function.
+    #
+    # Here is a helper function to make that more convenient
+    def account_view_get(self, use_invalid_token=False):
+        request = self.factory.get(
+            reverse('account',
+                    kwargs={'account_id': self.user.account.id}))
+        if (use_invalid_token):
+            request.META['HTTP_X_AUTH_TOKEN'] = 'bogus'
+        else:
+            request.META['HTTP_X_AUTH_TOKEN'] = self.user.token
+        return account(request, self.user.account.id)
+
+    def test_account_authorized(self):
+        """
+        Verify that the account view requires a valid token.
+        """
+
+        # First attempt to connect with an invalid token. This should fail.
+        response = self.account_view_get(use_invalid_token=True)
+        self.assertEquals(response.status_code, 401)
+
+        # Then, try again with the correct token, which should work
+        response = self.account_view_get()
+        self.assertEquals(response.status_code, 200)
